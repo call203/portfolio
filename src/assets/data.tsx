34842014996b30b85e23cca548d13f760c5d78b9
call203 <call203@daum.net>
2024-02-11 16:12:35 +0000

    Fix Sentence

--- src/assets/data.tsx
+++ src/assets/data.tsx
@@ -1,22 +1,14 @@
 export const Experiences = [
   {
-    title: 'SuperWalk',
-    name: 'SuperWalk App',
-    date: '04/2022 - 09/2022',
-    detail: [
-      '• Effectively collaborated with 2 blockchain developers and 2 backend developers to seamlessly integrate React Native code, ensuring efficiency',
-      '• Built this app within 3 months and released it, while also conducting testing with the participation of two frontend developers Effectively collaborated with 2 blockchain developers and 2 backend developers to seamlessly integrate React Native code, ensuring efficiency',
-      '• Actively participated in hiring front end developers and have assigned tasks to the team',
-    ],
-  },
-  {
     title: 'Proground',
-    name: 'Proground App',
-    date: '06/2021 - 05/2022',
+    name: 'Proground & SuperWalk App',
+    date: '06/2022 - 09/2022',
     detail: [
-      '• Effectively led a front-end team of 4 interns and improved code quality to enhance communication and ease of management',
-      '• Participated in building, releasing, and testing the app from the start and accommodated it for use by 10,000 users',
-      '• To utilize walking information, Handled various phone sensors and integrated API, such as Garmin, into the app',
+      '• Led the development of two major React Native applications, swiftly bring them to market, while closely collaborating with UX/UI and backend teams to ensure seamless integration and optimize user experience',
+      '• Actively engaged in interview processes to recruit and lead front end developers fostering a collaborative and high performing team environment',
+      '• Experienced in working in AGILE-based development environment and participating in business requirement gathering sessions with clients to ensure efficient project execution and alignment with organizational goals',
+      '• Developed and tested new features every week, updating iOS and Android applications. This resulted in over 10,000 installations from users.',
+      '• Researched and integrated GarminConnect Apis into the application, providing substantial real-time data support for users',
     ],
   },
   {
@@ -24,8 +16,8 @@ export const Experiences = [
     name: 'Forc Shopping App',
     date: '05/2021 - 07/2021',
     detail: [
-      '• Developed several functions, and seamlessly integrated them into the original code as part of a 3-person team.',
-      '• Developed the UI using React and implemented a search functionality that Connected Flask and React to efficiently retrieve a large amount of data for the search engine',
+      '• Create new features and developed UI using React Native for mobile applications, while leveraging React for the development of the admin web page',
+      '• Implemented a search functionality by integrating Flask and React to efficiently retrieve a large amount of data for the search engine',
     ],
   },
 ]
