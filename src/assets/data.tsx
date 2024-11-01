45940b507d84965287ac1ffed122911759f95324
call203 <call203@daum.net>
2024-11-01 16:40:56 +0900

    [FIX] update experience-ko

--- src/assets/data.tsx
+++ src/assets/data.tsx
@@ -1,25 +1,48 @@
 export const Experiences = [
   {
-    title: "Proground",
-    name: "Proground & SuperWalk App",
-    date: "06/2022 - 09/2022",
+    title: "프로그라운드",
+    name: "프로그라운드",
+    about:
+      "러닝 애플리케이션으로 시작해, 최근 Web 3.0 기술과 토큰 경제를 융합하여 새로운 피트니스 플랫폼으로 발전한 스타트업",
+    date: "2021.06 - 2022.09",
+    job: ["React/React Native 개발", "프론트엔드 리드"],
     detail: [
-      "• Led the development of two major React Native applications, swiftly bring them to market, while closely collaborating with UX/UI and backend teams to ensure seamless integration and optimize user experience",
-      "• Actively engaged in interview processes to recruit and lead front end developers fostering a collaborative and high performing team environment",
-      "• Experienced in working in AGILE-based development environment and participating in business requirement gathering sessions with clients to ensure efficient project execution and alignment with organizational goals",
-      "• Developed and tested new features every week, updating iOS and Android applications. This resulted in over 10,000 installations from users.",
-      "• Researched and integrated GarminConnect Apis into the application, providing substantial real-time data support for users",
-    ],
+      {
+        title: "프론트엔드 리드 및 다중 플랫폼 앱 개발",
+        info: "스타트업 환경에서 프론트엔드 개발을 전반적으로 리드하며, iOS와 Android 두 플랫폼에서 동시 운영되는 React Native 기반 애플리케이션을 신속하게 개발 및 출시. 이를 통해 다양한 기기에서 일관된 사용자 경험 제공."
+      },
+      {
+        title: "실시간 GPS추적 시스템 구축",
+        info: "지속적인 GPS 추적을 위한 실시간 데이터 처리 시스템을 구축하여 사용자 활동을 정확하게 기록하고, 실시간 피드백을 제공. 이를 통해 러닝 애플리케이션 핵심 기능인 실시간 위치 추적의 안정성과 정확도를 크게 향상 시킴."
+      },
+      {
+        title: "게임화된 애니메이션 효과 적용",
+        info: "동적인  UI 요소와 시각적 피드백을 제공해 사용자 참여를 유도하고 사용자 인터페이스를 직관적으로 개선하여 사용자 만족도를 크게 향상시킴."
+      },
+      {
+        title: "외부 API통합으로 기능 확장",
+        info: "Garmin 서비스 같은 외부 서비스와의 API 통합을 통해 애플리케이션의 기능을 확장하고, 사용자 맞춤형 실시간 데이터를 제공하는 시스템을 구축."
+      }
+    ]
   },
   {
-    title: "Forc Shopping",
-    name: "Forc Shopping App",
-    date: "03/2021 - 05/2021",
+    title: "포크홀딩스",
+    name: "포크홀딩스 (포크쇼핑)",
+    about:
+      "리뷰 커머스 플랫폼으로, 유튜브에서 제품별 리뷰 영상을 모아 최저가에 구매할 수 있는 애플리케이션을 제공하는 스타트업",
+    date: "2021.04 - 2021.05",
+    job: ["인턴", "React 개발", "Flask 이용한 검색기능"],
     detail: [
-      "• Create new features and developed UI using React Native for mobile applications, while leveraging React for the development of the admin web page",
-      "• Implemented a search functionality by integrating Flask and React to efficiently retrieve a large amount of data for the search engine",
-    ],
-  },
+      {
+        title: "관리자 웹 페이지 기능 개발",
+        info: "React를 이용해 관리자 제품 검색 및 등록 기능을 구축하여 운영 효율성을 개선."
+      },
+      {
+        title: "검색 기능 구축",
+        info: "Flask를 통해 대용량 데이터를 효율적으로 검색할 수 있는 기능을 구축하고, 이를 React Native 애플리케이션에 통합하여 사용자에게 신속한 데이터 검색 서비스를 제공."
+      }
+    ]
+  }
 ];
 
 export const Projects = [
@@ -27,18 +50,18 @@ export const Projects = [
     title: "Local Service Provider Connection Platform",
     short: "My SQL, NestJs, NextJs, Prisma, Git",
     info: "Website that allows users to search for, rate, and hire local service providers to work on a variety of personal projects",
-    git: "https://github.com/orgs/web-architectures-project/repositories",
+    git: "https://github.com/orgs/web-architectures-project/repositories"
   },
   {
     title: "Machine Learning Models of Gait Analysis",
     short: "Android, Java, Python, Firebase, Colab ",
     info: "A Study on the Method of Differentiating Between Elderly Walking and Non-Senior Walking Using Machine Learning Models",
-    site: "http://koreascience.or.kr/article/JAKO202128837704022.page",
+    site: "http://koreascience.or.kr/article/JAKO202128837704022.page"
   },
   {
     title: "User Experience Research - 4 Mobile apps design",
     short: "Figma",
     info: "Conducted research and surverys to identify service issues, define solutions, and establish userflow, while also designing UI/UX",
-    site: "https://soyeonwork.webflow.io/",
-  },
+    site: "https://soyeonwork.webflow.io/"
+  }
 ];
--- src/components/Company.tsx
+++ src/components/Company.tsx
@@ -1,52 +1,74 @@
-import { useState } from 'react'
-import { Experiences } from '../assets/data'
-import Carousel from 'react-multi-carousel'
-import 'react-multi-carousel/lib/styles.css'
+import { useState } from "react";
+import { Experiences } from "../assets/data";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 interface ExperiencesProps {
-  title?: string
-  name: string
-  date: string
-  detail: string[]
+  title?: string;
+  name: string;
+  date: string;
+  job: string[];
+  about: string;
+  detail: {
+    title: string;
+    info: string;
+  }[];
 }
 
 const CustomDot = ({ onMove, index, active, onClick }: any) => {
   return (
     <div
       className={`w-44 h-1.5 ${
-        active ? 'bg-black' : 'bg-white border border-shadow'
+        active ? "bg-black" : "bg-white border border-shadow"
       }`}
       onClick={() => onClick()}
     >
-      {'   '}
+      {"   "}
     </div>
-  )
-}
+  );
+};
 
-const Experience = ({ name, date, detail }: ExperiencesProps) => {
+const Experience = ({ name, date, detail, job, about }: ExperiencesProps) => {
   return (
     <div>
       <div className="font-bold text-lg mb-1">{name}</div>
-      <div className="font-light mb-6">{date}</div>
+      <div className="text-sm font-base mb-1">{about}</div>
+      <div className="font-light mb-1">{date}</div>
+      <div className="pb-4 flex flex-row flex-wrap">
+        {job.map((i) => {
+          return (
+            <div className="mt-2">
+              <span className="text-sm px-3 py-1 bg-black text-white rounded-full font-semibold mr-2">
+                {i}
+              </span>
+            </div>
+          );
+        })}
+      </div>
       <div>
         {detail.map((info, index) => {
           return (
-            <p className="pb-2 text-md" key={index}>
-              {info}
-            </p>
-          )
+            <div>
+              <p className="pb-1 text-md font-bold" key={index}>
+                {info.title}
+              </p>
+              <p className="pb-3 text-sm font-base" key={index}>
+                {info.info}
+              </p>
+            </div>
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
 function Company() {
-  const [companyIdx, setCompanyIdx] = useState(0)
+  const [companyIdx, setCompanyIdx] = useState(0);
 
   return (
     <div className="container font-sans">
-      <h1 className="font-bold text-2xl mb-16 text-center">Company</h1>
+      <h1 className="font-bold text-2xl mb-16 text-center">Experience</h1>
       <div className="hidden lg:flex flex-row">
         <div className="flex flex-row">
           <div className="flex-col">
@@ -56,23 +78,25 @@ function Company() {
                   key={index}
                   className={`py-3 w-40 mr-28 flex-col border-l-4  ${
                     companyIdx === index
-                      ? 'border-black font-bold'
-                      : 'border-gray-300'
+                      ? "border-black font-bold"
+                      : "border-gray-300"
                   } hover:font-bold`}
                   onClick={() => {
-                    setCompanyIdx(index)
+                    setCompanyIdx(index);
                   }}
                 >
                   <div className="text-left ml-3 text-lg">{item.title}</div>
                 </div>
-              )
+              );
             })}
           </div>
           <div>
             <Experience
               name={Experiences[companyIdx].name}
               date={Experiences[companyIdx].date}
+              job={Experiences[companyIdx].job}
               detail={Experiences[companyIdx].detail}
+              about={Experiences[companyIdx].about}
             />
           </div>
         </div>
@@ -93,31 +117,31 @@ function Company() {
           renderButtonGroupOutside={false}
           customDot={<CustomDot />}
           showDots
-          removeArrowOnDeviceType={['tablet', 'mobile']}
+          removeArrowOnDeviceType={["tablet", "mobile"]}
           slidesToSlide={1}
           swipeable
           responsive={{
             desktop: {
               breakpoint: {
                 max: 3000,
-                min: 1024,
+                min: 1024
               },
-              items: 1,
+              items: 1
             },
             mobile: {
               breakpoint: {
                 max: 464,
-                min: 0,
+                min: 0
               },
-              items: 1,
+              items: 1
             },
             tablet: {
               breakpoint: {
                 max: 1024,
-                min: 464,
+                min: 464
               },
-              items: 1,
-            },
+              items: 1
+            }
           }}
         >
           {Experiences.map((item, index) => {
@@ -126,14 +150,16 @@ function Company() {
                 name={item.name}
                 date={item.date}
                 detail={item.detail}
+                job={item.job}
+                about={item.about}
                 key={index}
               />
-            )
+            );
           })}
         </Carousel>
       </div>
     </div>
-  )
+  );
 }
 
-export default Company
+export default Company;
