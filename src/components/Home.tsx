3dc9e41157fbe2f984ae968ed64036bf7568bb49
call203 <call203@daum.net>
2024-11-07 21:18:46 +0900

    [FIX] update home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,7 @@ import { motion, useInView } from "framer-motion";
 import Avatar from "../assets/Avatar.png";
 import { Image } from "@chakra-ui/image";
 import { useRef } from "react";
+import DownImage from "../assets/Down.png";
 
 function Home() {
   const motionRef = useRef(null);
@@ -17,15 +18,34 @@ function Home() {
   };
 
   return (
-    <div className="container mx-auto font-sans">
-      <div className="flex justify-center">
+    <div className="container mx-auto font-sans flex flex-col justify-center item-center h-[70vh] relative  text-center">
+      <div className="">
+        <div className="md:text-8xl text-5xl font-extrabold md:mb-4 mb-2">
+          Hello, I'm
+        </div>
+        <div className="relative inline-block md:text-5xl text-2xl font-bold md:mb-4 mb-2 ">
+          <span className="relative z-10">직관적이고 편리함을 추구하는</span>
+          <span className="absolute left-0 bottom-0 w-full md:h-5 h-3 bg-yellow-300 opacity-70 rounded-sm z-0"></span>
+        </div>
+        <div className="md:text-5xl text-2xl font-bold md:mb-4 mb-2">
+          이소연 입니다
+        </div>
+        <div> Web & App Developer</div>
+      </div>
+
+      <div className="absolute bottom-0 w-full flex flex-col items-center text-gray-400 font-bold animate-bounce">
+        <p>Let's Scroll Down!</p>
+        <img src={DownImage} alt="scroll down" className="w-7 h-7" />
+      </div>
+
+      {/* <div className="flex justify-center bg-red-300">
         <Image
           src={Avatar}
           alt="profile img"
           className="h-auto max-w-full w-4/12"
         />
-      </div>
-      <div ref={motionRef}>
+      </div> */}
+      {/* <div ref={motionRef}>
         <div className="flex justify-center lg:text-xl text-lg">
           <motion.p
             variants={textVariants}
@@ -75,7 +95,7 @@ function Home() {
             추구합니다.
           </motion.p>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
